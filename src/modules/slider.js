695285b31208513697eaa44061f78335015d5ef0
krivonosoven82 <krivonosoven82@gmail.com>
2018-11-19 18:32:00 +0200

    yatip slider.0.0.2

--- src/modules/slider.js
+++ src/modules/slider.js
@@ -1,55 +1,68 @@
-let position = 0;
+
+    let position = 0;
 
 //----------arrayImg
-let arrayImg = [
-    './src/img/slider/cat.jpg',
-    './src/img/slider/edinorog.jpg',
-    './src/img/slider/hacker.jpg',
-    './src/img/slider/js.jpg',
-    './src/img/slider/spongebob.jpg',
-];
+    let arrayImg = [
+        './src/img/slider/cat.jpg',
+        './src/img/slider/edinorog.jpg',
+        './src/img/slider/hacker.jpg',
+        './src/img/slider/js.jpg',
+        './src/img/slider/spongebob.jpg',
+    ];
 
 //----------main container
-let contentItem = document.getElementsByClassName('content__body-item')[1];
-contentItem.className = 'content__body-item item-slider';
+    let contentItem = document.getElementsByClassName('content__body-item')[1];
+    contentItem.className = 'content__body-item item-slider';
+    console.log(contentItem.offsetWidth);
 
 //----------div slider
-const slider = document.createElement('div');
-slider.className = 'slider';
-contentItem.appendChild(slider);
+    let slider = document.createElement('div');
+    slider.className = 'slider';
+    contentItem.appendChild(slider);
+    console.log(slider.offsetHeight);
+
 
 //----------div slider__wrap
-let sliderWrap = document.createElement('div');
-sliderWrap.className = 'slider__wrap';
-slider.appendChild(sliderWrap);
+    let sliderWrap = document.createElement('div');
+    sliderWrap.className = 'slider__wrap';
+    slider.appendChild(sliderWrap);
 
 //----------img .slider__img
-for(let i = 0; i < arrayImg.length; i++){
-    let img = document.createElement('img');
-    img.setAttribute('src', arrayImg[i]);
-    img.setAttribute('alt', 'img');
-    img.className = 'slider__img';
-    sliderWrap.appendChild(img);
-}
-
-let indexImgSlider = document.querySelectorAll('.slider__img');
-
-
-console.dir(sliderWrap);
-console.log(sliderWrap);
-
-
-let stepPosition = (sliderWrap.offsetWidth / indexImgSlider.length);
-let sliderWrapWidth = position - (sliderWrap.offsetWidth - stepPosition);
-
-
-
-function nextSlide(){
-    if(position < sliderWrapWidth){
-        position = 0;
+    for(let i = 0; i < arrayImg.length; i++){
+        let img = document.createElement('img');
+        img.setAttribute('src', arrayImg[i]);
+        img.setAttribute('alt', 'img');
+        img.className = 'slider__img';
+        sliderWrap.appendChild(img);
     }
-    sliderWrap.style.left = position + 'px';
-    position = position - stepPosition;
-    setTimeout(nextSlide, 3000);
-}
-nextSlide();
+
+    let indexImgSlider = document.querySelectorAll('.slider__img');
+
+
+    let stepPosition = (sliderWrap.offsetWidth / indexImgSlider.length);
+    let sliderWrapWidth = position - (sliderWrap.offsetWidth - stepPosition);
+
+
+
+
+
+
+
+
+
+    function nextSlide(){
+        if(position < -2400){
+            position = 0;
+        }
+        sliderWrap.style.left = position + 'px';
+        position = position - 600;
+        setTimeout(nextSlide, 3000);
+    }
+
+
+
+
+
+
+    nextSlide();
+
